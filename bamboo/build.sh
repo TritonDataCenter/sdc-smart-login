2be725c0a02325192a9a13a1b15ef3699a0e3e43
John Sonnenschein <johns@joyent.com>
2011-07-15 13:01:11 -0700

    [AGENT-236] amon, CA and smartlogin should leave their artifacts in a configurable location

--- bamboo/build.sh
+++ bamboo/build.sh
@@ -10,6 +10,13 @@ BRANCH=$(git symbolic-ref HEAD | cut -d'/' -f3)
 DESCRIBE=$(git describe)
 BUILDSTAMP=`TZ=UTC date "+%Y%m%dT%H%M%SZ"`; export BUILDSTAMP
 PKG=${NAME}-${BRANCH}-${BUILDSTAMP}-${DESCRIBE}.tgz
+
+if [[ $( echo $BRANCH | grep release) && -z $PUBLISH_LOCATION ]]; then
+    releasedate=$(echo $BRANCH | cut -d '-' -f2)
+    RELEASEDIR=${releasedate:0:4}-${releasedate:4:2}-${releasedate:6:2}
+    PUBLISH_LOCATION=/rpool/data/coal/releases/${RELEASEDIR}/deps/
+fi
+
 if [[ -z $PUBLISH_LOCATION ]]; then
     PUBLISH_LOCATION=/rpool/data/coal/live_147/agents/smartlogin/${BRANCH}/
 fi

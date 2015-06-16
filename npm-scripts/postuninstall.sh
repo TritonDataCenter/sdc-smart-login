381e99f5f4e1b55a05825301e8a7f76e22fe5894
Joshua M. Clulow <jmc@joyent.com>
2015-06-16 01:23:33 -0700

    AGENT-909 for now, only agents using "config-agent" should register with SAPI

--- npm-scripts/postuninstall.sh
+++ npm-scripts/postuninstall.sh
@@ -6,8 +6,13 @@
 #
 
 #
-# Copyright (c) 2014, Joyent, Inc.
+# Copyright 2015 Joyent, Inc.
 #
+
+if [[ "${SDC_AGENT_SKIP_LIFECYCLE:-no}" = "yes" ]]; then
+    printf 'Running during package build; skipping lifecycle script.\n' >&2
+    exit 0
+fi
 
 export SMFDIR=$npm_config_smfdir
 
